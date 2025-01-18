b4d6a93d3fd6d5863b8df64d7720a75e30f5ee3d
haniagibus <hg.gibus@gmail.com>
2025-01-19 00:04:21 +0100

    updated app colors

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const secondary = Color(0xFF92B4F4);
   static const accent = Color(0xFFFFAA00);
   static const background = Color(0xFFFFFFFF);
-  static const button_background = Color(0xFFC8E3F9);
+  static const button_background = Color(0xFFDAE6FB);
   static const textPrimary = Color(0xFF212121); // Dark Gray
   static const textSecondary = Color(0xFF757575); // Medium Gray
 }
