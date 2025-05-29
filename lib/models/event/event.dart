8f0ac1c615fb369c356e21f6901954ddeae9efc2
haniagibus <hg.gibus@gmail.com>
2025-05-29 15:30:44 +0200

    backup before merge

--- lib/models/event/event.dart
+++ lib/models/event/event.dart
@@ -10,13 +10,16 @@ class Event {
   final EventType type;
   final DateTime dateTimeStart;
   final DateTime dateTimeEnd;
+
   //final String description;
   final String building;
   final String room;
   final List<Report> reports;
   bool isFavourite;
 
-  String get formattedDate => DateFormat('d MMM', 'pl_PL').format(dateTimeStart);
+  String get formattedDate =>
+      DateFormat('d MMM', 'pl_PL').format(dateTimeStart);
+
   String get formattedTime =>
       "${DateFormat('HH:mm', 'pl_PL').format(dateTimeStart)} - ${DateFormat('HH:mm', 'pl_PL').format(dateTimeEnd)}";
 
@@ -31,8 +34,8 @@ class Event {
     required this.building,
     required this.room,
     required this.reports,
-     this.isFavourite = false,
-   }) : id = id ?? Uuid().v4();
+    this.isFavourite = false,
+  }) : id = id ?? Uuid().v4();
 
   factory Event.fromJson(Map<String, dynamic> json) {
     return Event(
@@ -40,7 +43,7 @@ class Event {
       title: json['title'] ?? 'Brak tytułu',
       subtitle: json['subtitle'] ?? '',
       type: EventType.values.firstWhere(
-            (e) => e.toString() == 'EventType.${json['type']}',
+        (e) => e.toString() == 'EventType.${json['type']}',
         orElse: () => EventType.PlenarySession,
       ),
       dateTimeStart: DateTime.parse(json['dateTimeStart'] ?? ''),
@@ -50,25 +53,26 @@ class Event {
       room: json['room'] ?? '',
       reports: (json['reports'] != null && json['reports'] is List)
           ? (json['reports'] as List)
-          .map((reportJson) => Report.fromJson(reportJson))
-          .toList()
+              .map((reportJson) => Report.fromJson(reportJson))
+              .toList()
           : [],
       isFavourite: json['favourite'] ?? false,
     );
   }
 
-  Map<String, dynamic> toJson({bool includeId = true, bool includeReports = true}) => {
-    'id': id,
-    'title': title,
-    'subtitle': subtitle,
-    'type': type.toString().split('.').last,
-    'dateTimeStart': dateTimeStart.toIso8601String(),
-    'dateTimeEnd': dateTimeEnd.toIso8601String(),
-    //'description': description,
-    'building': building,
-    'room': room,
-    'reportsId':  reports.map((r) => r.id).toList(),
-    'favourite': isFavourite,
-  };
-
+  Map<String, dynamic> toJson(
+          {bool includeId = true, bool includeReports = true}) =>
+      {
+        'id': id,
+        'title': title,
+        'subtitle': subtitle,
+        'type': type.toString().split('.').last,
+        'dateTimeStart': dateTimeStart.toIso8601String(),
+        'dateTimeEnd': dateTimeEnd.toIso8601String(),
+        //'description': description,
+        'building': building,
+        'room': room,
+        'reportsId': reports.map((r) => r.id).toList(),
+        'favourite': isFavourite,
+      };
 }
