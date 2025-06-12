2f3d6af65f2e73c5482b5d1765cb05d954e3f1d0
weronika <weronikakoterba31@wp.pl>
2025-06-12 22:14:19 +0200

    star fix

--- lib/views/screens/schedule/event_screen.dart
+++ lib/views/screens/schedule/event_screen.dart
@@ -1,4 +1,5 @@
 // import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';  // <--- dodaj
 // import 'package:krit_app/models/event/event.dart';
 // import 'package:krit_app/theme/app_colors.dart';
 //
@@ -8,7 +9,7 @@
 // import '../../widgets/star_widget.dart';
 // import '../../../services/favourite_event_service.dart';
 //
-// class EventScreen extends StatefulWidget {
+// class EventScreen extends StatelessWidget {
 //   final Event event;
 //
 //   const EventScreen({
@@ -17,39 +18,23 @@
 //   });
 //
 //   @override
-//   State<EventScreen> createState() => _EventScreenState();
-// }
-//
-// class _EventScreenState extends State<EventScreen> {
-//   late bool isFavourite;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     isFavourite = FavoritesService.isFavorite(widget.event.id!);
-//     widget.event.isFavourite = isFavourite;
-//   }
-//
-//   Future<void> toggleFavourite() async {
-//     FavoritesService.toggleFavorite(widget.event.id!);
-//
-//     final updated = FavoritesService.isFavorite(widget.event.id!);
-//
-//     setState(() {
-//       isFavourite = updated;
-//       widget.event.isFavourite = updated;
-//     });
-//   }
-//
-//   @override
 //   Widget build(BuildContext context) {
+//     final favoritesService = Provider.of<FavoritesService>(context);
+//     final isFavourite = favoritesService.isFavorite(event.id!);
+//
+//     void toggleFavourite() {
+//       favoritesService.toggleFavorite(event.id!);
+//     }
+//
 //     return Scaffold(
 //       appBar: AppBar(
 //         backgroundColor: AppBarTheme().backgroundColor,
 //         leading: IconButton(
 //           icon: Icon(Icons.arrow_back),
 //           onPressed: () {
-//             Navigator.pop(context, isFavourite);
+//             final favoritesService = Provider.of<FavoritesService>(context, listen: false);
+//             final updated = favoritesService.isFavorite(event.id!);
+//             Navigator.pop(context, updated);  // zwróć aktualny stan!
 //           },
 //         ),
 //       ),
@@ -78,7 +63,7 @@
 //                             const SizedBox(width: 16),
 //                             Expanded(
 //                               child: Text(
-//                                 widget.event.title,
+//                                 event.title,
 //                                 style: const TextStyle(
 //                                   fontSize: 24,
 //                                   fontWeight: FontWeight.bold,
@@ -89,7 +74,7 @@
 //                             StarWidget(
 //                               isFavourite: isFavourite,
 //                               onTap: toggleFavourite,
-//                             )
+//                             ),
 //                           ],
 //                         ),
 //                       ),
@@ -110,15 +95,15 @@
 //                               children: [
 //                                 _buildCard(
 //                                     Icons.calendar_today,
-//                                     widget.event.formattedDate,
+//                                     event.formattedDate,
 //                                     AppColors.text_primary),
 //                                 _buildCard(
 //                                     Icons.access_time,
-//                                     widget.event.formattedTime,
+//                                     event.formattedTime,
 //                                     AppColors.accent),
-//                                 _buildCard(Icons.home, widget.event.building,
+//                                 _buildCard(Icons.home, event.building,
 //                                     AppColors.text_secondary),
-//                                 _buildCard(Icons.room, widget.event.room,
+//                                 _buildCard(Icons.room, event.room,
 //                                     AppColors.text_secondary),
 //                               ],
 //                             ),
@@ -133,25 +118,15 @@
 //                             ),
 //                             const SizedBox(height: 8),
 //                             Text(
-//                               widget.event.subtitle,
+//                               event.subtitle,
 //                               textAlign: TextAlign.justify,
 //                               style: const TextStyle(
 //                                 fontSize: 18,
 //                                 color: AppColors.text_secondary,
 //                               ),
 //                             ),
-//                             // const SizedBox(height: 8),
-//                             // const Divider(color: Colors.grey, thickness: 1),
-//                             // const SizedBox(height: 8),
-//                             // Text(
-//                             //   widget.event.description,
-//                             //   style: const TextStyle(
-//                             //     fontSize: 16,
-//                             //     color: AppColors.text_secondary,
-//                             //   ),
-//                             // ),
 //                             const SizedBox(height: 24),
-//                             _buildReportsSection(widget.event.reports),
+//                             _buildReportsSection(event.reports),
 //                           ],
 //                         ),
 //                       ),
@@ -227,9 +202,10 @@
 //     );
 //   }
 // }
+//
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';  // <--- dodaj
+import 'package:provider/provider.dart';
 import 'package:krit_app/models/event/event.dart';
 import 'package:krit_app/theme/app_colors.dart';
 
@@ -239,131 +215,124 @@ import '../../widgets/reports/report_tile.dart';
 import '../../widgets/star_widget.dart';
 import '../../../services/favourite_event_service.dart';
 
-class EventScreen extends StatelessWidget {
+class EventScreen extends StatefulWidget {
   final Event event;
 
-  const EventScreen({
-    super.key,
-    required this.event,
-  });
+  const EventScreen({super.key, required this.event});
+
+  @override
+  State<EventScreen> createState() => _EventScreenState();
+}
+
+class _EventScreenState extends State<EventScreen> {
+  late bool isFavourite;
+
+  @override
+  void initState() {
+    super.initState();
+    final favoritesService =
+    Provider.of<FavoritesService>(context, listen: false);
+    isFavourite = favoritesService.isFavorite(widget.event.id!);
+  }
+
+  void toggleFavourite() {
+    final favoritesService =
+    Provider.of<FavoritesService>(context, listen: false);
+    favoritesService.toggleFavorite(widget.event.id!);
+    setState(() {
+      isFavourite = favoritesService.isFavorite(widget.event.id!);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final favoritesService = Provider.of<FavoritesService>(context);
-    final isFavourite = favoritesService.isFavorite(event.id!);
-
-    void toggleFavourite() {
-      favoritesService.toggleFavorite(event.id!);
-    }
+    final event = widget.event;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppBarTheme().backgroundColor,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context, isFavourite);
+            Navigator.pop(context, isFavourite); // zwracamy aktualny stan
           },
         ),
       ),
-      body: Center(
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 700),
-            child: SafeArea(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 24.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 10),
-                      // Logo and Name
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Row(
-                          children: [
-                            ElementIcon(
-                              backgroundColor: AppColors.plenary_session,
-                              icon: Icons.event,
-                            ),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: Text(
-                                event.title,
-                                style: const TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: AppColors.text_primary,
-                                ),
-                              ),
-                            ),
-                            StarWidget(
-                              isFavourite: isFavourite,
-                              onTap: toggleFavourite,
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Divider(color: Colors.grey, thickness: 1),
-                            const SizedBox(height: 8),
-                            GridView.count(
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 4,
-                              mainAxisSpacing: 4,
-                              childAspectRatio: 1.6,
-                              children: [
-                                _buildCard(
-                                    Icons.calendar_today,
-                                    event.formattedDate,
-                                    AppColors.text_primary),
-                                _buildCard(
-                                    Icons.access_time,
-                                    event.formattedTime,
-                                    AppColors.accent),
-                                _buildCard(Icons.home, event.building,
-                                    AppColors.text_secondary),
-                                _buildCard(Icons.room, event.room,
-                                    AppColors.text_secondary),
-                              ],
-                            ),
-                            const SizedBox(height: 24),
-                            const Text(
-                              "Tytuł:",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: AppColors.text_primary,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              event.subtitle,
-                              textAlign: TextAlign.justify,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: AppColors.text_secondary,
-                              ),
-                            ),
-                            const SizedBox(height: 24),
-                            _buildReportsSection(event.reports),
-                          ],
-                        ),
-                      ),
-                    ],
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 24.0),
+        child: ListView(
+          children: [
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  ElementIcon(
+                    backgroundColor: AppColors.plenary_session,
+                    icon: Icons.event,
                   ),
-                ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Text(
+                      event.title,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.text_primary,
+                      ),
+                    ),
+                  ),
+                  StarWidget(
+                    isFavourite: isFavourite,
+                    onTap: toggleFavourite,
+                  ),
+                ],
               ),
             ),
-          ),
+            const Divider(color: Colors.grey, thickness: 1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                childAspectRatio: 1.6,
+                children: [
+                  _buildCard(Icons.calendar_today, event.formattedDate, AppColors.text_primary),
+                  _buildCard(Icons.access_time, event.formattedTime, AppColors.accent),
+                  _buildCard(Icons.home, event.building, AppColors.text_secondary),
+                  _buildCard(Icons.room, event.room, AppColors.text_secondary),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Tytuł:",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.text_primary,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    event.subtitle,
+                    style: const TextStyle(fontSize: 18, color: AppColors.text_secondary),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(height: 24),
+                  _buildReportsSection(event.reports),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -371,23 +340,13 @@ class EventScreen extends StatelessWidget {
 
   Widget _buildCard(IconData icon, String title, Color color) {
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(icon, size: 32, color: color),
           const SizedBox(height: 8),
-          Text(
-            title,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: color,
-            ),
-          ),
+          Text(title, style: TextStyle(fontWeight: FontWeight.bold, color: color)),
         ],
       ),
     );
@@ -399,30 +358,20 @@ class EventScreen extends StatelessWidget {
       children: [
         const Text(
           "Referaty:",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: AppColors.text_primary,
-          ),
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: AppColors.text_primary),
         ),
         const SizedBox(height: 8),
         if (reports.isEmpty)
-          const Text(
-            "Brak referatów dla tego wydarzenia",
-            style: TextStyle(fontSize: 16, color: AppColors.text_secondary),
-          )
+          const Text("Brak referatów dla tego wydarzenia", style: TextStyle(fontSize: 16, color: AppColors.text_secondary))
         else
           ListView.builder(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             itemCount: reports.length,
             itemBuilder: (context, index) {
-              final report = reports[index];
               return ReportTile(
-                report: report,
-                onTap: () {
-                  print("Wybrano referat: ${report.title}");
-                },
+                report: reports[index],
+                onTap: () => print("Wybrano referat: ${reports[index].title}"),
               );
             },
           ),
